8fdebf4b528048e4739c93ab150047fd63a01854
Jayme Fortes <Jayme.fortes@edu.pucrs.br>
2024-10-03 20:32:30 -0300

    Commit 4, inserção da Api exchangerates

--- script.js
+++ script.js
@@ -1,43 +1,65 @@
-function converter() {
+async function converter() {
     const valor = parseFloat(document.getElementById('valor').value);
     const moeda = document.getElementById('moeda').value;
-    const taxaRealPDolar = 0.20;
-    const taxaDolarPReal = 5.00;
-    const taxaRealPPeso = 17.713;
-    const taxaPesoPReal = 0.0056;
-
-    let resultado = 0;
     const resultadoDiv = document.getElementById('resultado');
 
     if (isNaN(valor)) {
-        document.getElementById("resultado").innerHTML = "Por favor, insira o valor novamente.";
+        resultadoDiv.innerHTML = "Por favor, insira o valor novamente.";
         resultadoDiv.style.color = "#ff0000"; // Mensagem de erro em vermelho
         return;
     }
 
-    switch (moeda) {
-        case "real":
-            resultado = valor * taxaRealPDolar;
-            resultadoDiv.innerHTML = "R$ " + valor.toFixed(2) + " é equivalente a $ " + resultado.toFixed(2) + " dólares.";
-            resultadoDiv.style.color = "#08d508";
-            break;
+    const apiKey = "1b15291b27fa3c27fbb50ade427d2d5d";
+    const url = `https://api.exchangeratesapi.io/latest?access_key=${apiKey}`;
 
-        case "dolar":
-            resultado = valor * taxaDolarPReal;
-            resultadoDiv.innerHTML = "$ " + valor.toFixed(2) + " é equivalente a R$ " + resultado.toFixed(2) + " reais.";
-            resultadoDiv.style.color = "#08d508";
-            break;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
 
-        case "peso":
-            resultado = valor * taxaPesoPReal;
-            resultadoDiv.innerHTML = "$ " + valor.toFixed(2) + " é equivalente a R$ " + resultado.toFixed(2) + " reais.";
-            resultadoDiv.style.color = "#08d508";
-            break;
+        let resultado = 0;
 
-        case "realParaPeso":
-            resultado = valor * taxaRealPPeso;
-            resultadoDiv.innerHTML = "R$ " + valor.toFixed(2) + " é equivalente a $ " + resultado.toFixed(2) + " pesos.";
-            resultadoDiv.style.color = "#08d508";
-            break;
+        switch (moeda) {
+            case "realParaDolar":
+                resultado = valor / data.rates.BRL * data.rates.USD; // Valor em reais para dólares
+                resultadoDiv.innerHTML = "R$ " + valor.toFixed(2) + " é equivalente a $ " + resultado.toFixed(2) + " dólares.";
+                resultadoDiv.style.color = "#08d508";
+                break;
+
+            case "dolarParaReal":
+                resultado = valor * data.rates.BRL; // Valor em dólares para reais
+                resultadoDiv.innerHTML = "$ " + valor.toFixed(2) + " é equivalente a R$ " + resultado.toFixed(2) + " reais.";
+                resultadoDiv.style.color = "#08d508";
+                break;
+
+            case "realParaPeso":
+                resultado = valor / data.rates.BRL * data.rates.MXN; // Valor em reais para pesos
+                resultadoDiv.innerHTML = "R$ " + valor.toFixed(2) + " é equivalente a $ " + resultado.toFixed(2) + " pesos.";
+                resultadoDiv.style.color = "#08d508";
+                break;
+
+            case "pesoParaReal":
+                resultado = valor / data.rates.MXN * data.rates.BRL; // Valor em pesos para reais
+                resultadoDiv.innerHTML = "$ " + valor.toFixed(2) + " é equivalente a R$ " + resultado.toFixed(2) + " reais.";
+                resultadoDiv.style.color = "#08d508";
+                break;
+
+            case "realParaEuro":
+                resultado = valor / data.rates.BRL * data.rates.EUR;
+                resultadoDiv.style.color = "#08d508";
+                resultadoDiv.innerHTML = "R$ "+valor.toFixed(2) + " é equivalente a € "+resultado.toFixed(2) + " euros";
+                break;
+
+            case "euroParaReal":
+                resultado = valor / data.rates.EUR * data.rates.BRL;
+                resultadoDiv.style.color = "#08d508";
+                resultadoDiv.innerHTML = "€ "+valor.toFixed(2) + " é equivalente a R$ " + resultado.toFixed(2) + " reais";
+        }
+    } catch (error) {
+        console.error(error);
+        resultadoDiv.innerHTML = "Erro ao obter dados da API.";
+        resultadoDiv.style.color = "#ff0000"; // Mensagem de erro em vermelho
     }
 }
